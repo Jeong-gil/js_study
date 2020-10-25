7ca8d01f72837b610bf8f194ef482869d5400286
YangJeongGil <bom20065@naver.com>
2020-10-25 13:21:39 +0900

    js study1

---
+++ main.js
@@ -0,0 +1,13 @@
+// whole-script strict mode syntax
+// JavaScript very flexible
+// flexible === dangerous
+// added ECMAScript 5
+
+// 바닐라 자바스크립트로 개발할 때, js코드 상단에 'use strict';를 선언해줄 것.
+// 선언하면 strict모드로 개발 가능
+// 유연한 자바스크립트 덕분에 좋은 점도 많지만 꼬일 경우 풀기가 쉽지 않기 때문.
+// 또한 strict모드를 사용하면 js엔진이 조금 더 효율적으로 js코드를 분석할 수 있음 (약간의 성능 개선 효과)
+
+
+'use strict';
+console.log('Hello World!');
