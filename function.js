5c8a4b574ebedd64a3bbcae27f5e017a80bf9185
YangJeongGil <bom20065@naver.com>
2020-10-29 23:38:00 +0900

    js study4_2

--- function.js
+++ function.js
@@ -91,3 +91,105 @@ function printMessage() {
 }
 printMessage();
 // console.log(massage);  // 지역변수라 에러발생
+// 이렇게 중첩된 함수에서 자식의 함수가 부모 함수에 정의된 변수들에 
+// 접근이 가능한 것들이 클로저(?)이다. 이 부분은 추후에 다룰 예정.
+
+
+// 6. Return a value
+// return 타입이 없는 함수도 사실은 return undefined; 이 생략되어 있음.
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2);
+console.log(`sum(1, 2) : ${sum(1,2)}`);
+
+
+// 7. Early return, Early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+// 조건이 맞지 않을 경우, 빨리 리턴을 해서 함수를 종료하고, 
+// 그 다음에 조건이 맞을 때, 필요한 로직들을 작성하는 것이 더 좋다.
+function upgradeUser2(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    // long upgrade logic...
+}
+
+
+// first-class function
+// functions are treated like any other variable
+// can be assignde as a value to variabel
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defiend. (hoisted)
+// a function expression is created when the excution reaches it.
+const print = function () {  // anonymous function
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+
+// 2. callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+// anonymous function
+const printYes = function () {
+    console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+const simplePrint = function () {
+    console.log('simplePrint!');
+};
+const simplePrint2 = () => console.log('simplePrint2!');
+
+const add = function (a, b) {
+    return a + b;
+};
+const add2 = (a, b) => a + b;
+
+// 한 줄인 경우, 블록 없이 작성이 가능하다.
+// 한 줄 이상인 경우 블록을 넣어서 표현이 가능하지만, return 키워드를 생략할 수 없다.
+const simpleMutiply = (a, b) => {
+    // do something more
+    return a * b;
+};
+
+// IIFE: Immediately Invoked Function Expression
+// JS에서 함수를 작성함과 동시에 바로 호출하고 싶을 때, 이렇게 가능하다.
+(function hello() {
+    console.log('IIFE');
+})();
+
+
+// Fun Quiz
+// function calculate(command, a, b)
+// command : add, substract, divide, multiply, remainder
